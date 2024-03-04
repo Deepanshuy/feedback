5097e2cdfd09d5f6e425cb422bd4fb771f7ea729
Deepanshuy <dipanshuy591@gmail.com>
2024-03-04 08:25:26 +0530

    saved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,154 @@
 import { Link } from "react-router-dom";
+import backgroundImage from "../../public/HomePagePics/backgroundImage.svg";
+import logo from "../../public/loginpics/logolkc.svg";
+import React, { useRef } from "react";
+import { Autoplay, Mousewheel, Keyboard } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 const HomePage = () => {
   return (
-    <div className="bg-[rgb(234,252,255);] h-screen w-[100%]">
-      <p className="bg-[rgb(234,252,255);] text-black absolute h-[15rem] w-[60%] top-20 left-80 text-5xl p-12">
-        
-         "Empower Educators: Enhance Teaching Through Feedback Hub"
-      </p>
-
-<div className="absolute z-10  top-80 w-[100%] flex justify-center items-center text-lg ">
-  <p>Select Your Respective Course</p>
-</div>
-      <div className="bg-green-200 h-[15rem]  w-[100%] absolute bottom-24 flex justify-around items-center">
-       <Link to="/btech" className="bg-[#40afbf] p-2 text-lg w-[15%] flex justify-center items-center rounded-lg cursor-pointer">
-         B.tech
-         </Link>
-         <Link to="/bvoc" className="bg-[#40afbf] p-2 text-lg w-[15%] flex justify-center items-center rounded-lg cursor-pointer">
-         B.voc
-         </Link>
-         <Link to="/bsc" className="bg-[#40afbf] p-2 text-lg w-[15%] flex justify-center items-center rounded-lg cursor-pointer">
-         B.sc
-         </Link>
+    <>
+      <div className="relative">
+        <img
+          src={backgroundImage}
+          alt="background"
+          className="object-cover md:h-auto h-60"
+        />
+        <div className="absolute left-[50%] top-10 md:w-[45%]   -translate-x-[50%] flex items-center justify-center flex-col gap-y-2 md:gap-y-6">
+          <img className="md:w-16 w-8" src={logo} alt="logo" />
+          <p className="md:text-5xl text-md w-full text-center text-white font-bold">
+            Lyallpur Khalsa College Technical Campus
+          </p>
+          <p className="text-white">Cantt.Road,Jalandhar</p>
+        </div>
+        <div className="absolute md:h-[3rem] h-[2rem] md:w-[12rem] w-[10rem] bg-[#35AFC0] -translate-x-[50%] left-[50%] -bottom-5  flex items-center justify-center rounded-xl text-white md:text-lg text-sm">
+          <p>Choose Your Stream</p>
         </div>
       </div>
-      
-      
-   
-  )
-}
+
+      <Swiper
+
+        className=" w-[95%] cursor-grab mx-auto my-[3rem] pl-[3rem]"
+        // install Swiper modules
+        Mousewheel={{
+          enabled: true,
+          forceToAxis: true,
+        }}
+        keyboard={{
+          enabled: true,
+          onlyInViewport: true,
+        }}
+        allowSlidePrev={true}
+        slidesPerView={4}
+        loop={true}
+        spaceBetween={10}
+        modules={[Mousewheel, Keyboard,Autoplay]}
+        autoplay={{
+          delay:3000,
+          disableOnInteraction:false
+        }}
+        style={{
+          "--swiper-navigation-size": "20px",
+        }}
+        freeMode={false}
+        rewind={false}
+        centeredSlides={false}  
+        navigation={false}
+        pagination={{ clickable: true }}
+            
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <div className="h-[12rem] w-[16rem] bg-red-200 rounded-2xl overflow-hidden ">
+            <div className="relative h-[100%] w-[100%]">
+              <img src="" alt="btech pic" />
+              <div className="absolute h-[1.5rem] bottom-0 left-[50%] -translate-x-[50%] flex items-center justify-center bg-gray-400 w-full py-2 opacity-40 z-10">
+               </div>
+              <p className="absolute bottom-1 left-[50%] -translate-x-[50%]">B.Tech</p>
+            </div>
+          </div>{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="h-[12rem] w-[16rem] bg-red-200 rounded-2xl overflow-hidden">
+            <div className="relative h-[100%] w-[100%]">
+              <img src="" alt="btech pic" />
+              <div className="absolute h-[1.5rem] bottom-0 left-[50%] -translate-x-[50%] flex items-center justify-center bg-gray-400 w-full py-2 opacity-40 z-10">
+               </div>
+              <p className="absolute bottom-1 left-[50%] -translate-x-[50%]">B.Sc</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="h-[12rem] w-[16rem] bg-red-200 rounded-2xl overflow-hidden">
+            <div className="relative h-[100%] w-[100%]">
+              <img src="" alt="btech pic" />
+              <div className="absolute h-[1.5rem] bottom-0 left-[50%] -translate-x-[50%] flex items-center justify-center bg-gray-400 w-full py-2 opacity-40 z-10">
+               </div>
+              <p className="absolute bottom-1 left-[50%] -translate-x-[50%]">B.Voc</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="h-[12rem] w-[16rem] bg-red-200 rounded-2xl overflow-hidden">
+            <div className="relative h-[100%] w-[100%]">
+              <img src="" alt="btech pic" />
+              <div className="absolute h-[1.5rem] bottom-0 left-[50%] -translate-x-[50%] flex items-center justify-center bg-gray-400 w-full py-2 opacity-40 z-10">
+               </div>
+              <p className="absolute bottom-1 left-[50%] -translate-x-[50%]">Diploma</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-[12rem] w-[16rem] bg-red-200 rounded-2xl overflow-hidden">
+            <div className="relative h-[100%] w-[100%]">
+              <img src="" alt="btech pic" />
+              <div className="absolute h-[1.5rem] bottom-0 left-[50%] -translate-x-[50%] flex items-center justify-center bg-gray-400 w-full py-2 opacity-40 z-10">
+               </div>
+              <p className="absolute bottom-1 left-[50%] -translate-x-[50%]">B.Tech</p>
+            </div>
+          </div>{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="h-[12rem] w-[16rem] bg-red-200 rounded-2xl overflow-hidden">
+            <div className="relative h-[100%] w-[100%]">
+              <img src="" alt="btech pic" />
+              <div className="absolute h-[1.5rem] bottom-0 left-[50%] -translate-x-[50%] flex items-center justify-center bg-gray-400 w-full py-2 opacity-40 z-10">
+               </div>
+              <p className="absolute bottom-1 left-[50%] -translate-x-[50%]">B.Sc</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="h-[12rem] w-[16rem] bg-red-200 rounded-2xl overflow-hidden">
+            <div className="relative h-[100%] w-[100%]">
+              <img src="" alt="btech pic" />
+              <div className="absolute h-[1.5rem] bottom-0 left-[50%] -translate-x-[50%] flex items-center justify-center bg-gray-400 w-full py-2 opacity-40 z-10">
+               </div>
+              <p className="absolute bottom-1 left-[50%] -translate-x-[50%]">B.Voc</p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="h-[12rem] w-[16rem] bg-red-200 rounded-2xl overflow-hidden">
+            <div className="relative h-[100%] w-[100%]">
+              <img src="" alt="btech pic" />
+              <div className="absolute h-[1.5rem] bottom-0 left-[50%] -translate-x-[50%] flex items-center justify-center bg-gray-400 w-full py-2 opacity-40 z-10">
+               </div>
+              <p className="absolute bottom-1 left-[50%] -translate-x-[50%]">Diploma</p>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
 
 export default HomePage;
--- src/pages/component/Navbar.jsx
+++ src/pages/component/Navbar.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { MdOutlineLogin } from "react-icons/md";
 import { GiNotebook } from "react-icons/gi";
+import feedbackLogo from "../../../public/logo/feedbackLogo.png"
 const Navbar = () => {
   return (
     <div className='h-[8dvh] w-screen fixed top-0 bg-[#40afbf] text-white font-medium flex justify-between items-center p-6 z-10'  >
-        <div className='p-2'>
-          <span className=' border-2 border-black bg-white rounded-md  p-1 text-black' > FEEDBACK HUB</span>
+        <div className='p-2 border-6 border-black h-[5rem] w-[12rem]'>
+         <img src={feedbackLogo} alt="feedbacklogo" className='object-cover h-full w-full ' />
         </div>
         <div className='md:flex items-center gap-x-4 hidden'>
              <Link to="/login" className='flex gap-x-1 justify-center items-center p-2'>
