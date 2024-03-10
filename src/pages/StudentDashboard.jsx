dbcd7a0c40edc609dad71ed2f914eca1dd60f886
Deepanshuy <dipanshuy591@gmail.com>
2024-03-10 06:41:45 +0530

    update

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -16,27 +16,195 @@ const StudentDashboard = () => {
 
   const data = [
     {
+      course: "btech",
+      branch: "cse",
       semester: "1st",
+      sess: "odd",
       subject: ["Physics", "ED & CG", "Maths I", "BEE"],
-      teacher: ["Teacher1", "Teacher2", "Teacher3", "Teacher4"],
+      teacher: ["1Teacher1", "1Teacher2", "1Teacher3", "1Teacher4"],
       metric: [
-        "Statement1",
-        "Statement2",
-        "Statement3",
-        "Statement4",
-        "Statement5",
+        "Has the Teacher covered entire Syllabus as prescribed by University/ College/Board?",
+        "Has the Teacher covered relevant topics beyond syllabus",
+        "Effectiveness of Teacher  in terms of: (a)Technical content/course content  (b)Communication skills  (c)Use of teaching aids",
+        "Pace on which content were covered",
+        "Motivation and inspiration for students to learn",
+        "Support for the development of students ‘ skill: (i)Practical demonstration (ii)Hands on training",
+        "Clarity of expectations of students",
+        "Feedback provided on students progress",
+        "Willingness to offer help and advice to students",
       ],
     },
     {
+      course: "btech",
+      branch: "cse",
       semester: "2nd",
+      sess: "even",
       subject: ["PPS", "Chemistry", "Maths II", "English"],
-      teacher: ["Teacher1", "Teacher2", "Teacher3", "Teacher4"],
+      teacher: ["2Teacher1", "2Teacher2", "2Teacher3", "2Teacher4"],
       metric: [
-        "Statement1",
-        "Statement2",
-        "Statement3",
-        "Statement4",
-        "Statement5",
+        "Has the Teacher covered entire Syllabus as prescribed by University/ College/Board?",
+        "Has the Teacher covered relevant topics beyond syllabus",
+        "Effectiveness of Teacher  in terms of: (a)Technical content/course content  (b)Communication skills  (c)Use of teaching aids",
+        "Pace on which content were covered",
+        "Motivation and inspiration for students to learn",
+        "Support for the development of students ‘ skill: (i)Practical demonstration (ii)Hands on training",
+        "Clarity of expectations of students",
+        "Feedback provided on students progress",
+        "Willingness to offer help and advice to students",
+      ],
+    },
+    {
+      course: "btech",
+      branch: "cse",
+      semester: "3rd",
+      sess: "odd",
+      subject: ["OOPS", "DSA", "Maths III", "FCH", "Digital Electronics"],
+      teacher: ["3Teacher1", "3Teacher2", "3Teacher3", "3Teacher4"],
+      metric: [
+        "Has the Teacher covered entire Syllabus as prescribed by University/ College/Board?",
+        "Has the Teacher covered relevant topics beyond syllabus",
+        "Effectiveness of Teacher  in terms of: (a)Technical content/course content  (b)Communication skills  (c)Use of teaching aids",
+        "Pace on which content were covered",
+        "Motivation and inspiration for students to learn",
+        "Support for the development of students ‘ skill: (i)Practical demonstration (ii)Hands on training",
+        "Clarity of expectations of students",
+        "Feedback provided on students progress",
+        "Willingness to offer help and advice to students",
+      ],
+    },
+    {
+      course: "btech",
+      branch: "cse",
+      semester: "4th",
+      sess: "even",
+      subject: [
+        "DM",
+        "DAA",
+        "UHV",
+        "COA",
+        "EVS",
+        "OS",
+        "DAA Lab",
+        "COA Lab",
+        "OS Lab",
+      ],
+      teacher: [
+        "Ms.Pooja Sonik",
+        "Ms.Pooja Rana",
+        "Er.Silky Khurana",
+        "Dr.Nancy Gupta",
+        "Er.Jasmine Kaur",
+        "Er.Rakesh Gagneja",
+        "Er. Taranpreet Kaur( G1) Er. Neha (G2)",
+        "Dr.Nancy Gupta",
+        "Er. Rakesh Gagneja",
+      ],
+      metric: [
+        "Has the Teacher covered entire Syllabus as prescribed by University/ College/Board?",
+        "Has the Teacher covered relevant topics beyond syllabus",
+        "Effectiveness of Teacher  in terms of: (a)Technical content/course content  (b)Communication skills  (c)Use of teaching aids",
+        "Pace on which content were covered",
+        "Motivation and inspiration for students to learn",
+        "Support for the development of students ‘ skill: (i)Practical demonstration (ii)Hands on training",
+        "Clarity of expectations of students",
+        "Feedback provided on students progress",
+        "Willingness to offer help and advice to students",
+      ],
+    },
+    {
+      course: "btech",
+      branch: "cse",
+      semester: "5th",
+      sess: "odd",
+      subject: ["PPS", "Chemistry", "Maths II", "English"],
+      teacher: ["5Teacher1", "5Teacher2", "5Teacher3", "5Teacher4"],
+      metric: [
+        "Has the Teacher covered entire Syllabus as prescribed by University/ College/Board?",
+        "Has the Teacher covered relevant topics beyond syllabus",
+        "Effectiveness of Teacher  in terms of: (a)Technical content/course content  (b)Communication skills  (c)Use of teaching aids",
+        "Pace on which content were covered",
+        "Motivation and inspiration for students to learn",
+        "Support for the development of students ‘ skill: (i)Practical demonstration (ii)Hands on training",
+        "Clarity of expectations of students",
+        "Feedback provided on students progress",
+        "Willingness to offer help and advice to students",
+      ],
+    },
+    {
+      course: "btech",
+      branch: "cse",
+      semester: "6th",
+      sess: "even",
+      subject: [
+        "AI",
+        "ML",
+        "CD",
+        "WCN",
+        "Cloud Computing",
+        "AI Lab",
+        "ML LAb",
+        "CD Lab",
+        "CC Lab",
+      ],
+      teacher: [
+        "Dr. Pooja Dhand",
+        "Er. Rajni Bedi",
+        "Dr. Sucharu",
+        "Er. Lovdeep",
+        "Dr. Rameshwar",
+        "Er. Varun Sharma",
+        "Er. Rajni Bedi",
+        "Dr. Sucharu",
+        "Dr. Rameshwar",
+      ],
+      metric: [
+        "Has the Teacher covered entire Syllabus as prescribed by University/ College/Board?",
+        "Has the Teacher covered relevant topics beyond syllabus",
+        "Effectiveness of Teacher  in terms of: (a)Technical content/course content  (b)Communication skills  (c)Use of teaching aids",
+        "Pace on which content were covered",
+        "Motivation and inspiration for students to learn",
+        "Support for the development of students ‘ skill: (i)Practical demonstration (ii)Hands on training",
+        "Clarity of expectations of students",
+        "Feedback provided on students progress",
+        "Willingness to offer help and advice to students",
+      ],
+    },
+    {
+      course: "btech",
+      branch: "cse",
+      semester: "7th",
+      sess: "odd",
+      subject: ["PPS", "Chemistry", "Maths II", "English"],
+      teacher: ["7Teacher1", "7Teacher2", "7Teacher3", "7Teacher4"],
+      metric: [
+        "Has the Teacher covered entire Syllabus as prescribed by University/ College/Board?",
+        "Has the Teacher covered relevant topics beyond syllabus",
+        "Effectiveness of Teacher  in terms of: (a)Technical content/course content  (b)Communication skills  (c)Use of teaching aids",
+        "Pace on which content were covered",
+        "Motivation and inspiration for students to learn",
+        "Support for the development of students ‘ skill: (i)Practical demonstration (ii)Hands on training",
+        "Clarity of expectations of students",
+        "Feedback provided on students progress",
+        "Willingness to offer help and advice to students",
+      ],
+    },
+    {
+      course: "btech",
+      branch: "cse",
+      semester: "8th",
+      sess: "even",
+      subject: ["PPS", "Chemistry", "Maths II", "English"],
+      teacher: ["8Teacher1", "8Teacher2", "8Teacher3", "8Teacher4"],
+      metric: [
+        "Has the Teacher covered entire Syllabus as prescribed by University/ College/Board?",
+        "Has the Teacher covered relevant topics beyond syllabus",
+        "Effectiveness of Teacher  in terms of: (a)Technical content/course content  (b)Communication skills  (c)Use of teaching aids",
+        "Pace on which content were covered",
+        "Motivation and inspiration for students to learn",
+        "Support for the development of students ‘ skill: (i)Practical demonstration (ii)Hands on training",
+        "Clarity of expectations of students",
+        "Feedback provided on students progress",
+        "Willingness to offer help and advice to students",
       ],
     },
   ];
@@ -45,7 +213,7 @@ const StudentDashboard = () => {
     e.preventDefault();
     console.log(formData);
   };
-
+  let session = "even";
   return (
     <div className="flex flex-col justify-between">
       <div className="text-3xl font-medium p-4">
@@ -56,7 +224,8 @@ const StudentDashboard = () => {
       <div className="p-4">
         {data.map(
           (item, index) =>
-            item.semester === user.sem && (
+            item.semester === user.sem &&
+            item.sess === session && (
               <form onSubmit={handleOnSubmit} key={index} className="flex-col">
                 <div className="flex flex-col md:flex-row gap-x-2 gap-y-6">
                   <FormControl size="small" fullWidth>
@@ -97,10 +266,10 @@ const StudentDashboard = () => {
                   </FormControl>
                 </div>
                 {formData.teacher && formData.subject && (
-                  <div className="p-4  flex flex-col gap-y-4 ">
+                  <div className="p-4  flex flex-col gap-y-4  ">
                     {item.metric.map((d, i) => (
                       <div
-                        className="flex gap-x-5 justify-between items-center"
+                        className="flex gap-x-5 justify-between items-center border-2 p-2"
                         key={i}
                       >
                         <p>{d}</p>
